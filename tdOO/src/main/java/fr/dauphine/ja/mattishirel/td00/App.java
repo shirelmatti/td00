1182aa7c90fbd213b2b4b958fdb3b0810bff209f
Shirel <shirelmatti@hotmail.fr>
2018-10-18 21:04:04 +0200

    premier commit

---
+++ tdOO/src/main/java/fr/dauphine/ja/mattishirel/td00/App.java
@@ -0,0 +1,14 @@
+package fr.dauphine.ja.mattishirel.td00;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Bonjour toi " );
+        
+    }
+}
